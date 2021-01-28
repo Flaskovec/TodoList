29a4f19641b11ac08338aa89e00774891f446c54
Flaskovec <lisnelox2@gmail.com>
2021-01-28 10:59:51 +0300

    #8

---
+++ src/state/todolists-reducer.test.ts
@@ -0,0 +1,80 @@
+import { v1 } from 'uuid';
+import { FilterValuesType, TodolistType } from '../App';
+import {
+  todolistReducer,
+  RemoveTodolistAC,
+  AddTodolistAC,
+  ChangeTodolisTitletAC,
+  ChangeTodolisFiltertAC,
+} from './todolists-reducer';
+
+test('correct todolist should be removed', () => {
+  let todolistId1 = v1();
+  let todolistId2 = v1();
+
+  const startState: Array<TodolistType> = [
+    { id: todolistId1, title: 'what to learn', filter: 'all' },
+    { id: todolistId2, title: 'what to buy', filter: 'all' },
+  ];
+
+  const endState = todolistReducer(startState, RemoveTodolistAC(todolistId1));
+
+  expect(endState.length).toBe(1);
+  expect(endState[0].id).toBe(todolistId2);
+});
+
+test('todolist should be added correctly', () => {
+  let todolistId1 = v1();
+  let todolistId2 = v1();
+
+  let newTodolistTitle = 'New Todolist';
+
+  const startState: Array<TodolistType> = [
+    { id: todolistId1, title: 'what to learn', filter: 'all' },
+    { id: todolistId2, title: 'what to buy', filter: 'all' },
+  ];
+
+  const endState = todolistReducer(startState, AddTodolistAC(newTodolistTitle));
+
+  expect(endState.length).toBe(3);
+  expect(endState[2].title).toBe(newTodolistTitle);
+  expect(endState[2].filter).toBe('all');
+});
+
+test('todolist should change its name correctly', () => {
+  let todolistId1 = v1();
+  let todolistId2 = v1();
+
+  let newTodolistTitle = 'New Todolist';
+
+  const startState: Array<TodolistType> = [
+    { id: todolistId1, title: 'what to learn', filter: 'all' },
+    { id: todolistId2, title: 'what to buy', filter: 'all' },
+  ];
+
+  const action = ChangeTodolisTitletAC(todolistId2, newTodolistTitle);
+
+  const endState = todolistReducer(startState, action);
+
+  expect(endState[0].title).toBe('what to learn');
+  expect(endState[1].title).toBe(newTodolistTitle);
+});
+
+test('filter of todolist should be changed correctly', () => {
+  let todolistId1 = v1();
+  let todolistId2 = v1();
+
+  let newFilter: FilterValuesType = 'completed';
+
+  const startState: Array<TodolistType> = [
+    { id: todolistId1, title: 'what to learn', filter: 'all' },
+    { id: todolistId2, title: 'what to buy', filter: 'all' },
+  ];
+
+  const action = ChangeTodolisFiltertAC(todolistId2, newFilter);
+
+  const endState = todolistReducer(startState, action);
+
+  expect(endState[0].filter).toBe('all');
+  expect(endState[1].filter).toBe(newFilter);
+});
---
+++ src/state/user-reducer.test.ts
@@ -0,0 +1,31 @@
+import { userReducer } from './user-reducer';
+
+test('user reducer should increment only age', () => {
+  const startState = { age: 20, childrenCount: 2, name: 'Vasya' };
+
+  const endState = userReducer(startState, { type: 'INCREMENT-AGE' });
+
+  expect(endState.age).toBe(21);
+  expect(endState.childrenCount).toBe(2);
+  expect(endState.name).toBe('Vasya');
+});
+
+test('user reducer should increment only childrenCount', () => {
+  const startState = { age: 20, childrenCount: 2, name: 'Vasya' };
+
+  const endState = userReducer(startState, { type: 'INCREMENT-CHILDREN-COUNT' });
+
+  expect(endState.age).toBe(20);
+  expect(endState.childrenCount).toBe(3);
+  expect(endState.name).toBe('Vasya');
+});
+
+test('user reducer should change only name', () => {
+  const startState = { age: 20, childrenCount: 2, name: 'Vasya' };
+  const newName = 'Victor';
+  const endState = userReducer(startState, { type: 'CHANGE-NAME', newName });
+
+  expect(endState.name).toBe(newName);
+  expect(endState.age).toBe(20);
+  expect(endState.childrenCount).toBe(2);
+});
