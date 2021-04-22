fe987a35a88a200ace8c42afa8066731eb03061d
Flaskovec <lisnelox2@gmail.com>
2021-04-22 13:22:52 +0300

    Updates

---
+++ precache-manifest.fa3dc0822d52f2b650d26e0bddf9aa14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f56e12c15d8b0ad3240ad34c7b08d566",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "ed21092ab59797e87f50",
+    "url": "/TodoList/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "262605af6d07031e30cc",
+    "url": "/TodoList/static/js/2.854275bc.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/TodoList/static/js/2.854275bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed21092ab59797e87f50",
+    "url": "/TodoList/static/js/main.ec219be5.chunk.js"
+  },
+  {
+    "revision": "2f7e94ddeafb28629bd3",
+    "url": "/TodoList/static/js/runtime-main.490e6b6e.js"
+  }
+]);
